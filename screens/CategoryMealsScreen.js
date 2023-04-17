98efd7e29e0321d1ab03da53f4439eb8640f4b0c
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-17 19:11:28 +0800

    6.24. Rendering a Meals List

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 export default function CategoryMealsScreen({ route, navigation }) {
   const catId = route.params.categoryId;
@@ -11,15 +12,23 @@ export default function CategoryMealsScreen({ route, navigation }) {
 
   function renderMealItem(itemData) {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        imageUrl={itemData.item.imageUrl}
+      />
     );
   }
 
   return (
     <View style={styles.screen}>
-      <FlatList data={displayedMeals} renderItem={renderMealItem} />
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 }
@@ -29,5 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 15,
   },
 });
