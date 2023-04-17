b4c8971e15af427a524811057d23e2410f377fae
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-17 10:09:17 +0800

    6.10. Navigating Between Screens
    https://reactnavigation.org/docs/navigating

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function CategoriesScreen(props) {
   return (
     <View style={styles.screen}>
       <Text>The Categories Screen!</Text>
+      <Button
+        title="Go to Meals!"
+        onPress={() => props.navigation.navigate("CategoryMeals")}
+      />
     </View>
   );
 }
