f663357ee31ba91fd6b9f3ea07985ffb9f0cb2cb
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-17 10:11:27 +0800

    6.13. Navigation to the Meal Details Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function CategoryMealsScreen(props) {
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen!</Text>
+      <Button
+        title="Go to Meal Detail!"
+        onPress={() => props.navigation.navigate("MealDetail")}
+      />
     </View>
   );
 }
