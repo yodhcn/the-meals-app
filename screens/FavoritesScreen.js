a827febb7b1fce35f97b812d30ca3ea985269db4
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-19 08:55:22 +0800

    6.33. Adding a Menu Button & Drawer Navigation

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+import { StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+// https://reactnavigation.org/docs/drawer-actions
+import { DrawerActions } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import CustomHeaderButton from "../components/HeaderButton";
 
-export default function FavoritesScreen(props) {
+export default function FavoritesScreen({ navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons left HeaderButtonComponent={CustomHeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id == "m2");
   return <MealList listData={favMeals} />;
 }
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useLayoutEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+// https://reactnavigation.org/docs/drawer-actions
+import { DrawerActions } from "@react-navigation/native";
 
-export default function FiltersScreen(props) {
+import CustomHeaderButton from "../components/HeaderButton";
+
+export default function FiltersScreen({ navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons left HeaderButtonComponent={CustomHeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.screen}>
       <Text>The Filters Screen!</Text>
